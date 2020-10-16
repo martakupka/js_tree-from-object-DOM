65240468d1756cdae90fcdb911d8f1b351d47901
martakupka <marta-k2008@ukr.net>
2020-10-16 16:00:17 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  for (const key in data) {
+    const list = document.createElement('ul');
+    const listElement = document.createElement('li');
+
+    listElement.innerText = key;
+
+    list.append(listElement);
+    element.append(list);
+
+    createTree(list, data[key]);
+  }
+}
+
+createTree(tree, food);
